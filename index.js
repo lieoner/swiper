d9538a27bd38ae7027d2d9b21e492a29d54c344d
Dmitry Mikhal <popcaric@gmail.com>
2022-05-07 01:23:14 +0300

    set up app ts

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
+
+import App from './src/app/core/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+function HeadlessCheck({isHeadless}) {
+    if (isHeadless) {
+        return null;
+    }
+
+    return <App />;
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
